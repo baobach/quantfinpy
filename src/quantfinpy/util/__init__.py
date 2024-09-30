437f76aeeed32b05c0cb093ecf58d0f075842b48
baobach <95854748+baobach@users.noreply.github.com>
2024-09-30 14:41:35 +0700

    feat: add fractional differential feature to the util module

--- src/quantfinpy/util/__init__.py
+++ src/quantfinpy/util/__init__.py
@@ -1,5 +1,9 @@
 """
-Utility functions. In particular Chapter20 code on Multiprocessing and Vectorization
+Utility functions used by other modules to provide functionality
+such as: multiprocessing, vectorization, fractional differentiation,
+etc.
+This module contains snippets from various chapters in the book.
+For detail, refer to the specific function in the module.
 """
 
 from quantfinpy.util.fast_ewma import ewma
@@ -7,4 +11,4 @@ from quantfinpy.util.multiprocess import expand_call, lin_parts, mp_pandas_obj, 
     report_progress
 from quantfinpy.util.generate_dataset import get_classification_data
 from quantfinpy.util.volatility import get_daily_vol, get_garman_class_vol, get_yang_zhang_vol, get_parksinson_vol
-
+from quantfinpy.util.frac_diff import get_weights, frac_diff, get_weights_ffd, frac_diff_ffd
